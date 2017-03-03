b4b7d83c38077ef4bf6bae6daf037e3df06cab17
Adam Schleter <adam.schleter@bigcommerce.com>
2017-03-03 11:11:14 +1100

    Allow configuration of custom_alert_output in ossec manager

--- manifests/server.pp
+++ manifests/server.pp
@@ -24,6 +24,7 @@ class ossec::server (
   $ossec_server_port                   = '1514',
   $ossec_syslog_server                 = false,
   $ossec_syslog_server_port            = '514',
+  $ossec_custom_alert_output           = '',
   $use_mysql                           = false,
   $mariadb                             = false,
   $mysql_hostname                      = undef,
